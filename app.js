5fb6f0c3f98a81c500f52174a4a3a294304dc92e
Surya-MERNstack <charusurya17@gmail.com>
2023-06-07 14:19:53 +0530

    .

--- app.js
+++ app.js
@@ -3,11 +3,11 @@ const path = require('path');
 
 const folderPath = 'files';
 const currentDate = new Date();
-const options = currentDate.toUTCString();
+const timestamp = currentDate.toUTCString();
 const filename = 'time.txt';
 const filePath = path.join(folderPath, filename);
 
 fs.ensureDir(folderPath)
-  .then(() => fs.appendFile(filePath, options, 'utf8'))
+  .then(() => fs.appendFile(filePath, timestamp, 'utf8'))
   .then(() => console.log(`Timestamp data added to ${timestamp} successfully.`))
   .catch((err) => console.error('Error adding timestamp data:', err));
