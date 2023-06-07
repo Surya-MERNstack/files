e71ce595a1b014bdfff1ba0d102fc94d8d913385
Surya-MERNstack <charusurya17@gmail.com>
2023-06-07 14:04:05 +0530

    some changes

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 const fs = require('fs-extra');
 const path = require('path');
 
-const folderPath = 'files'; // Specify the folder path including the parent directory
+const folderPath = 'files';
 const currentDate = new Date();
-const timestamp = currentDate.toUTCString();
+const options = { day: 'numeric', month: 'long', year: 'numeric' };
+const timestamp = currentDate.toLocaleDateString('en-US', options);
 const filename = 'time.txt';
 const filePath = path.join(folderPath, filename);
 
 fs.ensureDir(folderPath)
   .then(() => fs.appendFile(filePath, timestamp, 'utf8'))
-  .then(() => console.log(`Timestamp data added to ${filename} successfully.`))
+  .then(() => console.log(`Timestamp data added to ${timestamp} successfully.`))
   .catch((err) => console.error('Error adding timestamp data:', err));
